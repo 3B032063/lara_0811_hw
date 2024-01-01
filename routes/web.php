f68a7fb3d41153cd304a679eb2f5f265f33fd9b6
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-01 19:43:57 +0800

    設定route對應至controller的index方法

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home.index');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about.index');
 
 Route::get('/news', function () {
     return view('news');
-});
+})->name('news.index');
